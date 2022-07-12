d002f6669e86c0cfd2f00b05eff1415d6565d211
Joanluiz <joanluiz69@gmail.com>
2022-07-12 14:56:20 -0300

    concluido

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let comedy = [
+    title: 'Liar Liar',
+    director: ' Tom Shadyac',
+    year: '1997',
+]
+
+let action = [
+    title: 'Uncharted',
+    director: 'Ruben Fleischer',
+    year: '2022',
+]
+
+let fantasy = [
+    title: 'Spider Man No Way Home',
+    director: 'Jon Watts',
+    year: '2021',
+]
